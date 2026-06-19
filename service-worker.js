0c3269d6926bb7702d39abfb49bac136d868154a
EsqueleDev <178097595+EsqueleDev@users.noreply.github.com>
2026-06-19 10:29:10 -0300

    O pwa funciona agora :D
    E A PORRA DO CACHE FODE A TRANSMISSÃO E O CU DOS TEMAS

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,54 @@
-const CACHE_NAME = "radio-fantasma-v1";
+const CACHE_NAME = "offline-v1";
 
-const urlsToCache = [
-    "/",
+const OFFLINE_FILES = [
+    "/offline.html",
+    "/img/offline.jpg"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => cache.addAll(OFFLINE_FILES))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(async () => {
+
+                // Se for navegação (HTML)
+                if (event.request.mode === "navigate") {
+                    return caches.match("/offline.html");
+                }
+
+                // Se for imagem
+                if (event.request.destination === "image") {
+                    return caches.match("/img/offline.jpg");
+                }
+
+                return new Response("", {
+                    status: 503,
+                    statusText: "Offline"
+                });
+            })
     );
 });
